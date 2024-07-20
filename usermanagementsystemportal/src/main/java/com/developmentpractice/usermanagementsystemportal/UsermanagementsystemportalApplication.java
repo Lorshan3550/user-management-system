fca2a04b83c918aae477dd8c79fc296e68523b2d
Lorshan3550 <lorshannarayanasamy@gmail.com>
2024-07-20 17:52:46 +0530

    backend setup and configuration

---
+++ usermanagementsystemportal/src/main/java/com/developmentpractice/usermanagementsystemportal/UsermanagementsystemportalApplication.java
@@ -0,0 +1,13 @@
+package com.developmentpractice.usermanagementsystemportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementsystemportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementsystemportalApplication.class, args);
+	}
+
+}
---
+++ usermanagementsystemportal/src/test/java/com/developmentpractice/usermanagementsystemportal/UsermanagementsystemportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developmentpractice.usermanagementsystemportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementsystemportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
