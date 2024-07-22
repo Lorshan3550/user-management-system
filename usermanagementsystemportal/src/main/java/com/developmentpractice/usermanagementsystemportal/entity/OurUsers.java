23c67b7a91eb735c468cc21533cc8cdd4694170a
Lorshan3550 <lorshannarayanasamy@gmail.com>
2024-07-23 00:55:55 +0530

    add OurUsers entity with spring security

---
+++ usermanagementsystemportal/src/main/java/com/developmentpractice/usermanagementsystemportal/entity/OurUsers.java
@@ -0,0 +1,62 @@
+package com.developmentpractice.usermanagementsystemportal.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+//create entity as OurUser instaed of User -> we use user from spring security
+// @Data => Getter and Setter
+
+@Entity
+@Table(name = "ourusers")
+@Data
+public class OurUsers implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String name;
+    private String password;
+    private String city;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role)); // Role is determining the access to application
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        //return UserDetails.super.isAccountNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        //return UserDetails.super.isAccountNonLocked();
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        //return UserDetails.super.isCredentialsNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        //return UserDetails.super.isEnabled();
+        return true;
+    }
+}
